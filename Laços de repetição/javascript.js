52250f5b804a78da2e95a482c4dd4818c34b4ab8
Wesley.Ramox <60888784+WesleyRamox@users.noreply.github.com>
2020-05-10 21:09:07 -0300

    Update javascript.js

--- Laços de repetição/javascript.js
+++ Laços de repetição/javascript.js
@@ -1,11 +1,12 @@
 var numbers = "";
-// for(let i = 0; i < 10; i++) {
-//     numbers += i;
+for(let i = 0; i < 10; i++) {
+    numbers += i;
 
-//     if(i != 9) {
-//         numbers+= '-';
-//     }
-// }
+    if(i != 9) {
+        numbers+= '-';
+    }
+}
+
 var i = 0;
 while(i < 10) {
     numbers += i;
