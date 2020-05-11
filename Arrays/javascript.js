ffb7a636b965b8602e8706dada8e13ac9142b980
Wesley.Ramox <60888784+WesleyRamox@users.noreply.github.com>
2020-05-10 21:35:51 -0300

    Arrays I

---
+++ Arrays/javascript.js
@@ -0,0 +1,17 @@
+// FORMAL
+let myArr = new Array("Wesley", "Programador", 10);
+// console.log(myArr);
+
+// LITERAL
+let myArr2 = ["Wesley", "Estudante"];
+// console.log(myArr2);
+
+// ASSOCIATIVO
+let myArrAss = {
+    name: 'Wesley',
+    age: 14
+}
+// console.log(myArrAss);
+
+// console.log(myArr.length);
+console.log(myArr2.prototype);
