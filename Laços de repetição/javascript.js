afda9921475170369d0ddcb31a5d13b02ae69c5f
Wesley.Ramox <60888784+WesleyRamox@users.noreply.github.com>
2020-05-10 21:28:19 -0300

    Repetições

--- Laços de repetição/javascript.js
+++ Laços de repetição/javascript.js
@@ -1,20 +1,61 @@
-var numbers = "";
-for(let i = 0; i < 10; i++) {
-    numbers += i;
+// var numbers = "";
+// for(let i = 0; i < 10; i++) {
+//     numbers += i;
 
-    if(i != 9) {
-        numbers+= '-';
-    }
-}
+//     if(i != 9) {
+//         numbers+= '-';
+//     }
+// }
 
-var i = 0;
-while(i < 10) {
-    numbers += i;
+// var i = 0;
+// while(i < 10) {
+//     numbers += i;
 
-    if(i != 9) {
-        numbers+= '-';
-    }
+//     if(i != 9) {
+//         numbers+= '-';
+//     }
     
-    i++;
+//     i++;
+// }
+
+// do {
+//     numbers += i;
+//     if(i != 9) {
+//         numbers+= '-';
+//     }
+
+//     i++;
+// } while(i < 10);
+
+// console.log(numbers);
+
+// let person = {
+//     name: "Wesley",
+//     age: 14,
+//     city: "SP"
+// }
+
+// for(values in person) {
+//     console.log(person.name);
+// }
+
+let pessoas = {
+    aluno: {
+        nome: "Wesley",
+        age: 14
+    },
+    professor: {
+        nome: "Micael",
+        age: 34
+    }
 }
-console.log(numbers);
+
+for(key in pessoas) {
+    console.log(pessoas.professor);
+}
+
+let nomes = ['Wesley', 'Maicon', 'Vanderson', 'Cleiton'];
+
+nomes.forEach(names => {
+    console.log(names);
+});
